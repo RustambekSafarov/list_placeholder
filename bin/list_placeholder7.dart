95e54fb013e8299ff56b9649742f2cd997df2b24
Rustambek <rustambeksafarovv@gmail.com>
2022-07-15 16:31:17 +0500

    Redone

--- bin/list_placeholder7.dart
+++ bin/list_placeholder7.dart
@@ -6,4 +6,18 @@ Return the List
 
 */
 
-void main() {}
+List func(int M,int N, int K){
+    int i = M;
+    int y = 0;
+    List x = [];
+    while(N>i){
+    x.add(i);
+    i+= K;
+
+    }
+    return x;
+}
+
+void main() {
+    print(func(4,10,2));
+}
