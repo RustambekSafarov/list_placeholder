c1b30499b1258df7870c336624c06494d3049128
Rustambek <rustambeksafarovv@gmail.com>
2022-07-15 23:55:05 +0500

    Redone

--- bin/list_placeholder10.dart
+++ bin/list_placeholder10.dart
@@ -6,4 +6,31 @@ Return the List
 
 */
 
-void main() {}
+List func(int M,int N){
+    int i = M;
+    int y = 0;
+    List x = [];
+    String z = '';
+    while(N>i){
+        if(i%2==0){
+            z+="'";
+            z = "'"+i.toString()+"'";
+            
+            x.add(z);
+            
+            
+            
+        }else {
+            x.add(i);
+        }
+        i++;
+    }
+        
+
+    
+    return x;
+}
+
+void main() {
+    print(func(4,11));
+}
