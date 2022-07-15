19dea1602dd1da5f6c34daa58a7a368dae790626
Rustambek <rustambeksafarovv@gmail.com>
2022-07-15 16:59:06 +0500

    Redone

--- bin/list_placeholder9.dart
+++ bin/list_placeholder9.dart
@@ -5,5 +5,21 @@ Argument int n
 Return the List
 
 */
+List func(int M,int N){
+    int i = M;
+    int y = 0;
+    List x = [];
+    while(N>i){
+        if(i%2==1){
+            x.add(i);
 
-void main() {}
+        }
+        i++;
+
+    }
+    return x;
+}
+
+void main() {
+    print(func(4,11));
+}
