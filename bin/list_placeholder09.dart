1cc3699652da73bbfce33745011a9e5f9644ef72
Rustambek <rustambeksafarovv@gmail.com>
2022-07-16 09:13:52 +0500

    Redone

---
+++ bin/list_placeholder09.dart
@@ -0,0 +1,25 @@
+/*
+Create function func 
+Create a list of n elements with odd numbers.
+Argument int n
+Return the List
+
+*/
+List func(int M,int N){
+    int i = M;
+    int y = 0;
+    List x = [];
+    while(N>i){
+        if(i%2==1){
+            x.add(i);
+
+        }
+        i++; 
+
+    }
+    return x;
+}
+
+void main() {
+    print(func(4,11));
+}
