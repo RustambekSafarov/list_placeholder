d3deca7c09e9d0a6864b6afd1e6b335ebe7600d7
Rustambek <rustambeksafarovv@gmail.com>
2022-07-16 09:12:06 +0500

    Redone

---
+++ bin/list_placeholder07.dart
@@ -0,0 +1,23 @@
+/*
+Create function func 
+Create a list ranging from M to N with a step of K.
+Argument int M, N, K
+Return the List
+
+*/
+
+List func(int M,int N, int K){
+    int i = M;
+    int y = 0;
+    List x = [];
+    while(N>i){
+    x.add(i); 
+    i+= K;
+
+    }
+    return x;
+}
+
+void main() {
+    print(func(4,10,2));
+}
